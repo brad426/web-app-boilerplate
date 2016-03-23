dbace19cb5e11c400f5819fa7790416b17ed1e8e
Brad Botha <brad426@gmail.com>
2016-03-23 23:14:24 +0200

    fixed up return stream in copy task

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,23 +1,25 @@
 'use strict';
 
 var gulp = require('gulp');
-var size = require('gulp-size');
+var merge = require('merge-stream');
 
 gulp.task('copy', function () {
 
   // Modernizr is not built into our application's JavaScript, make sure it gets copied over
-  gulp.src('src/js/vendor/modernizr-respond-js.js')
+  var modernizr = gulp.src('src/js/vendor/modernizr-respond-js.js')
     .pipe(gulp.dest(global.destination + '/js/vendor'));
 
-  return gulp.src([
-      'src/**/*',
-      // Exclude the following:
-      '!src/*.html', // html files are handled by html task
-      '!src/{js,js/**}', // scripts are handled by scripts task
-      '!src/{img,img/**}', // handled by image task
-      '!src/{scss,scss/**}', // handled by styles task
-      '!src/{layout,layout/**}', // we don't need layout
-      '!src/{include,include/**}' // we don't need include
-    ])
-    .pipe(gulp.dest(global.destination));
+  var staticAssets = gulp.src([
+    'src/**/*',
+    // Exclude the following:
+    '!src/*.html', // html files are handled by html task
+    '!src/{js,js/**}', // scripts are handled by scripts task
+    '!src/{img,img/**}', // handled by image task
+    '!src/{scss,scss/**}', // handled by styles task
+    '!src/{partial,partial/**}' // we don't need partial
+  ])
+  .pipe(gulp.dest(global.destination));
+
+  return merge(modernizr, staticAssets);
+  
 });
