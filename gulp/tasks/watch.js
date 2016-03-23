23da624aa6857e6c4cdf99f78858abd9f960da14
Brad Botha <brad426@gmail.com>
2016-03-23 23:16:10 +0200

    no need to manually watch JS
    watchify handles updates

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,7 +6,7 @@ gulp.task('watch', function() {
 
   gulp.watch(['src/*.html', 'src/layout/**/*.html', 'src/include/**/*.html'],  ['html']);
   gulp.watch('src/scss/**/*.scss',  ['styles']);
-  gulp.watch('src/js/**/*.js',  ['scripts']);
   gulp.watch('src/img/**/*',  ['images']);
+  // JS changes are monitored via watchify in scripts.js
 
 });
