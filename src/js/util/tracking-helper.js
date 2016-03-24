609a078d13abbfd26a7cac926fd83e04b7817633
Brad Botha <brad426@gmail.com>
2016-03-24 22:50:52 +0200

    tracking helper is now just an object literal

--- src/js/util/tracking-helper.js
+++ src/js/util/tracking-helper.js
@@ -1,23 +1,10 @@
-/**
- * Don't instantiate with 'new' keyword.
- * This class returns a single instance of the tracking helper.
- */
-var TrackingHelper = function() {
-    
-}
+module.exports = {
+	trackEvent: function(category, action, label, value) {
+	    ga('send', 'event', category, action, label, value);
+	},
 
-TrackingHelper.prototype = {
-
-    constructor: TrackingHelper,
-
-    trackEvent: function(category, action, label, value) {
-        ga('send', 'event', category, action, label, value);
-    },
-
-    trackVPage: function(name) {
-        name = name.replace(/\!/g, '');
-        ga('send', 'pageview', name + '.html');
-    }
-}
-
-module.exports = new TrackingHelper();
+	trackVPage: function(name) {
+	    name = name.replace(/\!/g, '');
+	    ga('send', 'pageview', name + '.html');
+	}
+};
